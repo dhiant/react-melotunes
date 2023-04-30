068200d97bf37f6e85cc4c70dd65a308cf479c60
sushant dhimal <sushant07.dhimal@gmail.com>
2023-04-30 22:17:42 +0545

    project setup & add login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+const Login = () => {
+  return (
+    <div className="grid place-items-center h-screen bg-primary">
+      <img src="/assets/logo.jpg" alt="spotify_logo" className="h-40" />
+      <a
+        href=""
+        className="uppercase p-5 font-medium text-white no-underline rounded-full bg-green"
+      >
+        login with spotify{" "}
+      </a>
+    </div>
+  );
+};
+
+export default Login;
