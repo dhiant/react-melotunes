ff9a833f8b10a7e36cdefb4d65908e089b4471b2
sushant dhimal <sushant07.dhimal@gmail.com>
2023-05-02 20:58:03 +0545

    add: fetch user spotify profile data

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import Login from "./components/Login";
 
 function App() {
   const [token, setToken] = useState(null);
+  const [userProfile, setUserProfile] = useState("");
 
+  async function fetchUserSpotifyProfile(_token) {
+    const result = await fetch("https://api.spotify.com/v1/me", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${_token}` },
+    });
+    return await result.json();
+  }
+
+  console.log(userProfile, "userprofile");
   useEffect(() => {
     const _token = getAccessTokenFromURL();
     window.location.hash = "";
-    console.log("token", token);
     if (_token) {
       setToken(_token);
+      fetchUserSpotifyProfile(_token).then((res) => setUserProfile(res));
     }
   }, [token]);
 
